09bf411107a04b603312270eb633649b7e426c55
xavimazo <34321000+xavimazo@users.noreply.github.com>
2019-01-26 21:57:36 -0300

    Commit

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -79,8 +79,25 @@ namespace BackOffice.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(
-        Include = "ProductId,ProductName,ProductHeight,ProductWidth,ProductDeep,ProductDiameter,ProductWeight,ProductOrigin,ProductMicrowave,ProductMainIcon,OriginId,CapacityId,CategorieId,LineId")] Product product)
+        Include = "ProductId,ProductName,ProductHeight,ProductWidth,ProductDeep,ProductDiameter,ProductWeight,ProductOrigin,ProductMicrowave,ProductMainIcon,OriginId,CapacityId,CategorieId,LineId")] Product product, int[] Colors, int[] Parts)
         {
+            if (Colors != null)
+            {
+                var dbColors = from c in db.Colors
+                               join d in Colors
+                               on c.ColorId equals d
+                               select c;
+                product.Colors = dbColors.ToList();
+            }
+            if (Parts != null)
+            {
+                var dbParts = from c in db.Parts
+                              join d in Parts
+                              on c.PartId equals d
+                              select c;
+                product.Parts = dbParts.ToList();
+            }
+
             if (ModelState.IsValid)
             {
                 db.Products.Add(product);
@@ -104,20 +121,17 @@ namespace BackOffice.Controllers
                 return HttpNotFound();
             }
             var PModel = new ProductCreateEditViewModel();
-            using (var db = new TPW())
-            {
-                PModel.ShortDescriptions = db.ShortDescriptions.ToList();
-                PModel.FullDescriptions = db.FullDescriptions.ToList();
-                PModel.Observations = db.Observations.ToList();
-                PModel.Recommendations = db.Recommendations.ToList();
-                PModel.Capacities = db.Capacities.ToList();
-                PModel.Categories = db.Categories.ToList();
-                PModel.Colors = db.Colors.ToList();
-                PModel.Lines = db.Lines.ToList();
-                PModel.Origins = db.Origins.ToList();
-                PModel.Parts = db.Parts.ToList();
-                PModel.Product = product;
-            }
+            PModel.ShortDescriptions = db.ShortDescriptions.ToList();
+            PModel.FullDescriptions = db.FullDescriptions.ToList();
+            PModel.Observations = db.Observations.ToList();
+            PModel.Recommendations = db.Recommendations.ToList();
+            PModel.Capacities = db.Capacities.ToList();
+            PModel.Categories = db.Categories.ToList();
+            PModel.Colors = db.Colors.ToList();
+            PModel.Lines = db.Lines.ToList();
+            PModel.Origins = db.Origins.ToList();
+            PModel.Parts = db.Parts.ToList();
+            PModel.Product = product;
             return View(PModel);
         }
 
@@ -126,8 +140,58 @@ namespace BackOffice.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProductId,ProductName,ProductHeight,ProductWidth,ProductDeep,ProductDiameter,ProductWeight,ProductOrigin,ProductMicrowave,ProductMainIcon,OriginId,CapacityId,CategorieId,LineId")] Product product)
+        public ActionResult Edit([Bind(Include = "ProductId,ProductName,ProductHeight,ProductWidth,ProductDeep,ProductDiameter,ProductWeight,ProductOrigin,ProductMicrowave,ProductMainIcon,OriginId,CapacityId,CategorieId,LineId")] Product product, int[] Colors, int[] Parts)
         {
+            var originalProduct = db.Products.Find(product.ProductId);
+            if (Colors != null)
+            {
+                var removedColors = db.Colors.Where(c => c.Products.Any(p => p.ProductId == originalProduct.ProductId) && !Colors.Contains(c.ColorId));
+                foreach (var col in removedColors)
+                {
+                    if (db.Entry(col).State == EntityState.Detached)
+                        db.Colors.Attach(col);
+
+                    originalProduct.Colors.Remove(col);
+                    col.Products.Remove(originalProduct);
+                }
+
+                var dbColors = from c in db.Colors
+                               join d in Colors
+                               on c.ColorId equals d
+                               select c;
+
+                var addedColors = dbColors.ToList().Where(c => !originalProduct.Colors.Contains(c));
+                foreach (var col in addedColors)
+                {
+                    if (db.Entry(col).State == EntityState.Detached)
+                        db.Colors.Attach(col);
+
+                    originalProduct.Colors.Add(col);
+                    col.Products.Add(originalProduct);
+                }
+            }
+            else
+            {
+                var removedColors = db.Colors.Where(c => c.Products.Any(p => p.ProductId == originalProduct.ProductId));
+                foreach (var col in removedColors)
+                {
+                    if (db.Entry(col).State == EntityState.Detached)
+                        db.Colors.Attach(col);
+
+                    originalProduct.Colors.Remove(col);
+                    col.Products.Remove(originalProduct);
+                }
+            }
+            //if (Parts != null)
+            //{
+            //    var dbParts = from c in db.Parts
+            //                  join d in Parts
+            //                  on c.PartId equals d
+            //                  select c;
+
+            //    originalProduct.Parts = dbParts.ToList();
+            //}
+
             if (ModelState.IsValid)
             {
                 db.Entry(product).State = EntityState.Modified;
